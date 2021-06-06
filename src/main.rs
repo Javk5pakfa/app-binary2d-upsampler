bdb88db1f9e5f35b11630831623c57589867d32a
Jack Hu <zhongth@g.clemson.edu>
2021-06-06 11:26:06 +0800

    Add .cbor file read functionality

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 use anyhow::Result;
-use clap::{App as clapApp, Arg};
 use binary2d::*;
+use clap::{App as clapApp, Arg, ArgMatches};
+// use glob::glob;
 
 
-fn main() -> Result<()> {
-
-    let default_file_name = "upsampled.cbor";
+fn argument_parse(default_name: &str) -> ArgMatches {
 
     let matches = clapApp::new("Upsample Config")
         .author("Zhongtian H. <zhongth@clemson.edu>")
@@ -16,15 +15,43 @@ fn main() -> Result<()> {
         .arg(Arg::with_name("OUTPUT")
             .short("o")
             .help("Output file name")
-            .default_value(default_file_name)
+            .default_value(default_name)
             .index(2))
         .get_matches();
 
+    matches
+}
+
+
+fn main() -> Result<()> {
+
+    let default_file_name = "chkpt.upsampled.cbor";
+    let matches = argument_parse(default_file_name);
+
     let f = matches.value_of("INPUT").unwrap();
     let out_f = matches.value_of("OUTPUT").unwrap();
+    // let old_file_path = glob(&String::from(f))?;
+    // let new_file_path = glob(&String::from(out_f))?;
 
+    // Gather data from old checkpoint file.
     let file: app::App = binary2d::io::read_cbor(f)?;
-    io::write_cbor(&file, out_f)?;
+    let mut new_file = file.clone();
+    let old_bs = file.config.mesh.block_size;
+    let nb = file.config.mesh.num_blocks;
+    let new_bs = old_bs * 2;
+
+    // Print useful info.
+    println!("Checkpoint file provided is {}", f);
+    println!("Output file will be named {}", out_f);
+    println!("Number of blocks is {}", nb);
+    println!("Old block size is {}", old_bs);
+    println!("New block size will be {}", new_bs);
+
+    // Update the new file.
+    new_file.config.mesh.block_size = new_bs;
+
+    // Write updated checkpoint file to directory.
+    io::write_cbor(&new_file, out_f)?;
 
     Ok(())
 }
